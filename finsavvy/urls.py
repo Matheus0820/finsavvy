d11d0356b445eeb837da06b6382db8f5fe04d850
Matheus0820 <mr7052954@gmail.com>
2025-07-23 14:39:35 -0300

    feat: Modificando o base.html, configurando o login e logout, adicionando o html do Dashboard

--- finsavvy/urls.py
+++ finsavvy/urls.py
@@ -1,9 +1,10 @@
 # example/urls.py
 from django.urls import path
 
-from finsavvy.views import dashboard
+from finsavvy.views import dashboard, aviso
 
 
 urlpatterns = [
-    path('', dashboard),
+    path('', aviso),
+    path('dashboard/', dashboard, name='dashboard'),
 ]
--- finsavvy/views.py
+++ finsavvy/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render
 from datetime import datetime
 
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
+def aviso(request):
+    return render(request, "pages/aviso_em_desenvolvimento.html")
+
+@login_required
 def dashboard(request):
     # now = datetime.now()
-    return render(request, "pages/aviso_em_desenvolvimento.html") # base.html
+    return render(request, "pages/dashboard.html")
